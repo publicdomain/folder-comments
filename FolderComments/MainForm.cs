9826b6e95ae6efcb35052c8ea4189128f1bbb6b8
Victor <publicdomainvic@gmail.com>
2023-02-24 12:42:48 -0400

    Add OnFreeReleasesPublicDomainisToolStripMenuItemClick()

--- FolderComments/MainForm.cs
+++ FolderComments/MainForm.cs
@@ -174,7 +174,8 @@ namespace FolderComments
         /// <param name="e">Event arguments.</param>
         private void OnFreeReleasesPublicDomainisToolStripMenuItemClick(object sender, EventArgs e)
         {
-            // TODO Add code
+            // Open our website
+            Process.Start("https://publicdomain.is");
         }
 
         /// <summary>
