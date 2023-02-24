f28648b7fcac8d0c59391daca552647a2d266575
Victor <publicdomainvic@gmail.com>
2023-02-24 19:44:17 -0400

    Add OnAboutToolStripMenuItemClick()

--- FolderComments/MainForm.cs
+++ FolderComments/MainForm.cs
@@ -30,6 +30,11 @@ namespace FolderComments
         private Icon associatedIcon = null;
 
         /// <summary>
+        /// The associated icon bitmap.
+        /// </summary>
+        private Bitmap associatedIconBitmap = null;
+
+        /// <summary>
         /// The context menu text.
         /// </summary>
         private string contextMenuText = "Edit comments";
@@ -38,11 +43,6 @@ namespace FolderComments
         /// The registry key list.
         /// </summary>
         private List<string> registryKeyList = new List<string>();
-
-        /// <summary>
-        /// The associated icon bitmap.
-        /// </summary>
-        private Bitmap associatedIconBitmap = null;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="T:FolderComments.MainForm"/> class.
@@ -207,7 +207,52 @@ namespace FolderComments
         /// <param name="e">Event arguments.</param>
         private void OnAboutToolStripMenuItemClick(object sender, EventArgs e)
         {
-            // TODO Add code        
+            // Set license text
+            var licenseText = $"CC0 1.0 Universal (CC0 1.0) - Public Domain Dedication{Environment.NewLine}" +
+                $"https://creativecommons.org/publicdomain/zero/1.0/legalcode{Environment.NewLine}{Environment.NewLine}" +
+                $"Libraries and icons have separate licenses.{Environment.NewLine}{Environment.NewLine}" +
+                $"Talk icon by inspire-studio - Pixabay License{Environment.NewLine}" +
+                $"https://pixabay.com/vectors/talk-chat-comment-communication-6931551/{Environment.NewLine}{Environment.NewLine}" +
+                $"Patreon icon used according to published brand guidelines{Environment.NewLine}" +
+                $"https://www.patreon.com/brand{Environment.NewLine}{Environment.NewLine}" +
+                $"GitHub mark icon used according to published logos and usage guidelines{Environment.NewLine}" +
+                $"https://github.com/logos{Environment.NewLine}{Environment.NewLine}" +
+                $"DonationCoder icon used with permission{Environment.NewLine}" +
+                $"https://www.donationcoder.com/forum/index.php?topic=48718{Environment.NewLine}{Environment.NewLine}" +
+                $"PublicDomain icon is based on the following source images:{Environment.NewLine}{Environment.NewLine}" +
+                $"Bitcoin by GDJ - Pixabay License{Environment.NewLine}" +
+                $"https://pixabay.com/vectors/bitcoin-digital-currency-4130319/{Environment.NewLine}{Environment.NewLine}" +
+                $"Letter P by ArtsyBee - Pixabay License{Environment.NewLine}" +
+                $"https://pixabay.com/illustrations/p-glamour-gold-lights-2790632/{Environment.NewLine}{Environment.NewLine}" +
+                $"Letter D by ArtsyBee - Pixabay License{Environment.NewLine}" +
+                $"https://pixabay.com/illustrations/d-glamour-gold-lights-2790573/{Environment.NewLine}{Environment.NewLine}";
+
+            // Prepend sponsors
+            licenseText = $"RELEASE SUPPORTERS:{Environment.NewLine}{Environment.NewLine}* Jesse Reichler{Environment.NewLine}* Max P.{Environment.NewLine}* Kathryn S.{Environment.NewLine}* Cranioscopical{Environment.NewLine}{Environment.NewLine}=========={Environment.NewLine}{Environment.NewLine}" + licenseText;
+
+            // Set title
+            string programTitle = typeof(MainForm).GetTypeInfo().Assembly.GetCustomAttribute<AssemblyTitleAttribute>().Title;
+
+            // Set version for generating semantic version
+            Version version = typeof(MainForm).GetTypeInfo().Assembly.GetName().Version;
+
+            // Set about form
+            var aboutForm = new AboutForm(
+                $"About {programTitle}",
+                $"{programTitle} {version.Major}.{version.Minor}.{version.Build}",
+                $"Made for: mykemo{Environment.NewLine}DonationCoder.com{Environment.NewLine}Day #55, Week #08 @ February 24, 2023",
+                licenseText,
+                this.Icon.ToBitmap())
+            {
+                // Set about form icon
+                Icon = this.associatedIcon,
+
+                // Set always on top
+                TopMost = this.TopMost
+            };
+
+            // Show about form
+            aboutForm.ShowDialog();
         }
 
         /// <summary>
